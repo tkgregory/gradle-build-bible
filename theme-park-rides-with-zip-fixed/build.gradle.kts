3b662ea59b4198e645cd21511fcadf1742afcedf
Tom Gregory <t.k.gregory@gmail.com>
2023-07-19 19:08:47 +0100

    Migrate theme-park-rides-with-zip-fixed to Kotlin.

--- theme-park-rides-with-zip-fixed/build.gradle.kts
+++ theme-park-rides-with-zip-fixed/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
     base
 }
 
-tasks.register<Copy>("generateDescriptions") {
+val generateDescriptions = tasks.register<Copy>("generateDescriptions") {
     from("descriptions")
     into("$buildDir/descriptions")
     filter<ReplaceTokens>("tokens" to mapOf("THEME_PARK_NAME" to "Grelephant's Wonder World"))
 }
 
 tasks.register<Zip>("zipDescriptions") {
-    from("$buildDir/descriptions")
+    from(generateDescriptions)
     destinationDirectory.set(buildDir)
     archiveFileName.set("descriptions.zip")
 }
