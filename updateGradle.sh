6e4f06dad71ae27c4d451873588489355931fd7f
Tom Gregory <t.k.gregory@gmail.com>
2023-08-04 10:01:52 +0100

    Update Gradle to 8.2.1 in all projects & add update script.

---
+++ updateGradle.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No version supplied"
+    exit 1
+fi
+
+declare -a StringArray=(
+"file-diff-plugin"
+"gradle-in-the-real-world/code-coverage"
+"gradle-in-the-real-world/code-quality"
+"gradle-in-the-real-world/code-style"
+"gradle-in-the-real-world/docker-image"
+"theme-park-api"
+"theme-park-manager"
+"theme-park-manager-refactored"
+"theme-park-rides"
+"theme-park-rides-status"
+"theme-park-rides-with-zip"
+"theme-park-rides-with-zip-fixed"
+ )
+for directory in ${StringArray[@]}; do
+  echo "Updating $directory Gradle version to $1"
+  cd $directory > /dev/null
+  ./gradlew wrapper -q --gradle-version $1 && ./gradlew -q > /dev/null
+  cd - > /dev/null
+done
